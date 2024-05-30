714d59d3d7b6f62fe0eff94550ad2f64ae947299
Lunitur <karlo.puselj@gmail.com>
2024-05-30 22:15:34 +0200

    nvidia stable victus

--- hosts/victus/nvidia.nix
+++ hosts/victus/nvidia.nix
@@ -81,7 +81,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta; #config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.stable; #config.boot.kernelPackages.nvidiaPackages.stable;
 
     prime = {
       sync.enable = true;
