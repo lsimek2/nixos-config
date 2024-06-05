1b13b5c8671eaf3e0631e464dacb49a9cdf2c0ea
lsimek <iinjano@proton.me>
2024-06-05 18:00:43 +0200

    nixvim basic

--- modules/nixvim.nix
+++ modules/nixvim.nix
@@ -9,5 +9,43 @@
     extraPlugins = with pkgs.vimPlugins; [
       vim-nix
     ];
-  };
+
+		colorschemes.gruvbox.enable = true;
+		plugins = {
+			lualine.enable = true;
+			telescope.enable = true;
+			oil.enable = true;
+			treesitter.enable = true;
+			luasnip.enable = true;
+		};
+	
+		plugins.lsp = {
+			enable = true;
+	
+			servers = {
+				ruff.enable = true;
+			};
+		};
+	
+		plugins.cmp = {
+			enable = true;
+			autoEnableSources = true;
+	
+# 			sources = [
+# 				{name = "nvim-lsp";}
+# 				{name = "path";}
+# 				{name = "buffer";}
+# 			];
+		};
+	
+		options = {
+			number = true;
+			relativenumber = true;
+	
+			shiftwidth = 2;
+	
+		};
+	
+	  globals.mapleader = " ";
+	};
 }
