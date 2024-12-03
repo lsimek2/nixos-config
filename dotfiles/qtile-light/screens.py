7353ef25bc1941e9c4601d7a2387de393ddd272b
Lunitur <karlo.puselj@gmail.com>
2024-12-03 06:21:43 +0100

    qtile random wallpapaer

--- dotfiles/qtile-light/screens.py
+++ dotfiles/qtile-light/screens.py
@@ -1,9 +1,18 @@
 from libqtile.config import Screen
-
 from bars import main_bar, main_bar2
 
+import os
+import random
+
+wallpaper_dir = './Nextcloud/wallpapers'
+
+def get_random_wallpaper():
+    wallpapers = os.listdir(wallpaper_dir)
+    random_wallpaper = random.choice(wallpapers)
+    return os.path.join(wallpaper_dir, random_wallpaper)
+
 main_screen = Screen(top=main_bar,
-  wallpaper="./Nextcloud/autic.jpg",
+  wallpaper=get_random_wallpaper(),
   wallpaper_mode="fill")  # Options: 'fill', 'stretch', 'center'
 main_screen2 = Screen(top=main_bar2)
 
