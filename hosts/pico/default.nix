8968bf293a6c2b35ffe63d18231cfd5c49cfe007
Lunitur <karlo.puselj@gmail.com>
2024-12-04 06:21:49 +0100

    nextcloud hostname update

--- hosts/pico/default.nix
+++ hosts/pico/default.nix
@@ -14,23 +14,36 @@
   environment.systemPackages = with pkgs; [
     helix
   ];
- 
+
   services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
     forceSSL = true;
     sslCertificate = "/etc/private/pico.akita-bleak.ts.net.crt";
     sslCertificateKey = "/etc/private/pico.akita-bleak.ts.net.key";
   };
 
-  services.nextcloud = {                
-    enable = true;                   
+  services.nextcloud = {
+    enable = true;
+    package = pkgs.nextcloud30;
     # Instead of using pkgs.nextcloud28Packages.apps,
     # we'll reference the package version specified above
     hostName = "pico.akita-bleak.ts.net";
     https = true;
     config = {
-     adminpassFile = "/etc/private/nextcloud-admin-pass";
-     adminuser = "root"; };
-    settings.trusted_domains = [ "pico" ];
+      adminpassFile = "/etc/private/nextcloud-admin-pass";
+      adminuser = "root";
+    };
+    # settings.trusted_domains = [ "pico" ];
+    settings = let
+      prot = "https"; # or https
+      host = "pico.akita-bleak.ts.net";
+      dir = "/";
+    in {
+      overwriteprotocol = prot;
+      overwritehost = host;
+      overwritewebroot = dir;
+      overwrite.cli.url = "${prot}://${host}${dir}/";
+      htaccess.RewriteBase = dir;
+    };
   };
 
   boot.loader.systemd-boot.enable = true;
@@ -39,9 +52,15 @@
   # boot.kernelPackages = pkgs.linuxPackages_zen;
 
   networking.hostName = "pico";
-  networking.firewall = { 
-    allowedUDPPorts = [ 80 443 ];
-    allowedTCPPorts = [ 80 443 ];
+  networking.firewall = {
+    allowedUDPPorts = [
+      80
+      443
+    ];
+    allowedTCPPorts = [
+      80
+      443
+    ];
   };
 
   system.stateVersion = "24.05";
