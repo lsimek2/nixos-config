b37a2724e76d20d36dd2e5f515becaf223506a75
Lunitur <karlo.puselj@gmail.com>
2025-01-04 04:53:08 +0100

    mako

--- users/carjin/dotfiles.nix
+++ users/carjin/dotfiles.nix
@@ -3,7 +3,7 @@
   home.file.".config/nushell/completers.nu".source = ../../dotfiles/nushell/completers.nu;
   home.file.".config/kmonad".source = ../../dotfiles/kmonad;
   home.file.".config/dunst".source = ../../dotfiles/dunst;
-  home.file.".config/mako/config".source = ../../dotfiles/mako/config;
+  # home.file.".config/mako/config".source = ../../dotfiles/mako/config;
   # home.file.".config/sway".source = ../../dotfiles/sway;
   # home.file.".config/swaylock".source = ../../dotfiles/swaylock;
   home.file.".config/waybar".source = ../../dotfiles/waybar;
--- users/carjin/home.nix
+++ users/carjin/home.nix
@@ -88,6 +88,23 @@
     };
   };
 
+  services.mako = {
+    enable = true;
+    anchor = "bottom-right";
+    # font = "monospace 10";
+    # backgroundColor = "#000000";
+    # textColor = "#ff0000";
+    width = 350;
+    margin = "0,20,20";
+    # padding = 10;
+    borderSize = 1;
+    # borderColor = "#7DDA58";
+    borderRadius = 5;
+    defaultTimeout = 10000;
+    groupBy = "summary";
+    icons = true;
+  };
+
   home.sessionVariables = {
     EDITOR = "hx";
   };
